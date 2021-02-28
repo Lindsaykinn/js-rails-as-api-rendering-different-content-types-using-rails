8c55183597541a77b33876e2407ffdcb88e005d6
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-28 13:44:19 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,15 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    # render 'birds/index.html.erb'
+    # render plain: "Hello, #{@birds[3].name}"
+    # render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+    # render json: {message: 'Hashes of data will get converted to JSON'}
+    # render json: ['As', 'well', 'as', 'arrays']
+    # render json: @birds
+    # render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds']}
+    # instance variables are no longer needed bc we are not viewing through ERB so we can just use a local variable (same as @birds = Bird.all on line 3):
+    render json: birds
+    # --or explicit rendering-- render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds']}.to_json
   end
 end
